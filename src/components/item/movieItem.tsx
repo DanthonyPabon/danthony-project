24f9f42319b762619ad35b33c5abbddc916ca42b
Danthony Pabon <danthonypabong@gmail.com>
2023-02-06 15:05:07 +0100

    Movie items component have been added

---
+++ src/components/item/movieItem.tsx
@@ -0,0 +1,40 @@
+import SubTitle from "components/text/subTitle";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import FastImage from "react-native-fast-image";
+import dimensions from "styles/dimensions";
+import { IMovieItem } from "types/components";
+
+const styles = StyleSheet.create({
+  content: {
+    width: 130,
+  },
+  image: {
+    borderRadius: dimensions.margin,
+    height: 200,
+    width: 130,
+  },
+});
+
+const renderImage = (urlImage: string) => {
+  return (
+    <FastImage
+      style={styles.image}
+      source={{ uri: urlImage }}
+      resizeMode={FastImage.resizeMode.stretch}
+    />
+  );
+};
+
+const MovieItem = ({ urlImage, title, colors }: IMovieItem) => {
+  return (
+    <View style={styles.content}>
+      {renderImage(urlImage)}
+      <View style={{ marginTop: dimensions.generalTop }}>
+        <SubTitle colors={colors} txt={title} />
+      </View>
+    </View>
+  );
+};
+
+export default MovieItem;
