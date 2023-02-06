88289c4870ed96747f6a9e575f798ddd043fe100
Danthony Pabon <danthonypabong@gmail.com>
2023-02-06 15:05:36 +0100

    Carousel component have been added

---
+++ src/components/carousel/carousel.tsx
@@ -0,0 +1,49 @@
+import MovieItem from "components/item/movieItem";
+import React, { FunctionComponent } from "react";
+import {
+  ListRenderItem,
+  FlatList,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import dimensions from "styles/dimensions";
+import { IMovies } from "types/api";
+import { ICarouselProps } from "types/components";
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: dimensions.margin,
+  },
+  marginR16: {
+    marginRight: dimensions.margin,
+  },
+});
+
+const Carousel: FunctionComponent<ICarouselProps> = ({
+  movies,
+  colors,
+  onPress,
+}) => {
+  const renderItem: ListRenderItem<IMovies> = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => onPress(item)} style={styles.marginR16}>
+        <MovieItem colors={colors} urlImage={item.image} title={item.title} />
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.content}>
+      <FlatList
+        data={movies}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+};
+
+export default Carousel;
