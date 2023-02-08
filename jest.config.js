25af8a121fb1c338bd90da041699a3980b3b2409
Danthony Pabon <danthonypabong@gmail.com>
2023-02-08 15:41:02 +0100

    config + test libraries

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   preset: "react-native",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
